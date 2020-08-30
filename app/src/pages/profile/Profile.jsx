79b1d44a83312ddb1a6ca0381adeea6aa7cd29ec
giorgia <g.sambrotta@gmail.com>
2020-08-30 14:48:12 +0200

    implement camera stream, display shot and clear shot

--- app/src/pages/profile/Profile.jsx
+++ app/src/pages/profile/Profile.jsx
@@ -1,27 +1,79 @@
 /* eslint-disable global-require */
-import React from 'react';
+import 'regenerator-runtime/runtime'
+import React, { useEffect, useState } from 'react'
+import { Layout } from '../../components/layout'
+import { Button, Flex } from '../../components/styles'
+import { Camera, Wrapper, Error, Video, Canvas } from './style'
 
-import { Layout } from '../../components/layout';
-import { Button, Flex } from '../../components/styles';
-import { Camera, Wrapper } from './style';
+const CAMERA_HEIGHT = 400
+const CAMERA_WIDTH = 300
 
-const Profile = () => (
-  <Layout title="Take a snapshot">
-    <Flex>
-      <Wrapper>
-        <Camera>
-          Webcam stream shows here
-        </Camera>
-        <Button className="primary">Take shot</Button>
-      </Wrapper>
-      <Wrapper>
-        <Camera>
-          uploaded snapshot shows here
-        </Camera>
-        <Button className="green">Clear</Button>
-      </Wrapper>
-    </Flex>
-  </Layout>
-);
+const Profile = () => {
+  const [errorMsg, setErrorMsg] = useState('none')
+  const canvas = React.createRef()
+  const streamContainer = React.createRef()
 
-export default Profile;
+  useEffect(() => {
+    async function streamVideo() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        })
+
+        streamContainer.current.srcObject = stream
+        streamContainer.current.play()
+      } catch (error) {
+        console.log(`Error streaming video: ${error}`)
+        setErrorMsg('block')
+      }
+    }
+
+    streamVideo()
+  }, [])
+
+  function handleShot() {
+    const video = streamContainer.current
+
+    canvas.current.width = CAMERA_WIDTH
+    canvas.current.height = CAMERA_HEIGHT
+    canvas.current
+      .getContext('2d')
+      .drawImage(video, -150, 0, 550, CAMERA_HEIGHT)
+
+    const img = canvas.current.toDataURL('image/jpeg')
+    console.log('img', img)
+  }
+
+  function handleClearShot() {
+    const context = canvas.current.getContext('2d')
+    context.clearRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT)
+  }
+
+  return (
+    <Layout title='Take a snapshot'>
+      <Error display={errorMsg}>Please enable access and attach a camera</Error>
+      <Flex>
+        <Wrapper>
+          <Camera>
+            Webcam stream shows here
+            <Video playsinline autoplay ref={streamContainer}></Video>
+          </Camera>
+          <Button className='primary' onClick={handleShot}>
+            Take shot
+          </Button>
+        </Wrapper>
+        <Wrapper>
+          <Camera>
+            uploaded snapshot shows here
+            <Canvas ref={canvas}></Canvas>
+          </Camera>
+          <Button className='green' onClick={handleClearShot}>
+            Clear
+          </Button>
+        </Wrapper>
+      </Flex>
+    </Layout>
+  )
+}
+
+export default Profile
--- app/src/pages/profile/style.js
+++ app/src/pages/profile/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Camera = styled.div`
   height: 400px;
@@ -6,15 +6,35 @@ export const Camera = styled.div`
   color: #ffffff;
   font: bold 18px/24px 'Source sans pro';
   text-align: center;
-  margin: 20px  0;
-`;
+  margin: 20px 0;
+  position: relative;
+  overflow: hidden;
+`
+
+export const Video = styled.video`
+  height: 400px;
+  width: 300px;
+  object-fit: cover;
+`
+
+export const Canvas = styled.canvas`
+  height: 400px;
+  width: 300px;
+  object-fit: cover;
+`
 
 export const Wrapper = styled.div`
   width: 300px;
   margin: 10px;
-`;
+`
 
 export const Flex = styled.div`
   display: flex;
-  justify-content: space-between
-`;
+  justify-content: space-between;
+`
+
+export const Error = styled.p`
+  display: ${(props) => props.display};
+  text-align: center;
+  font: bold 14px/20px 'Source sans pro';
+`
